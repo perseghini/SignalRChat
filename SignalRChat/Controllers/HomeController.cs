0ab700791477afb4611be467550f18843875b8e6
Ludovic Perseghini <perseghini@gmail.com>
2015-04-13 16:27:57 -0700

    Add new Chat Page

---
+++ SignalRChat/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace SignalRChat.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SignalRChat/Hubs/ChatHub.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SignalRChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            Clients.All.addNewMessageToPage(Clients.Caller.userName, message);
+        }
+
+        public void Connected()
+        {
+            //Send all client new user connected
+            Clients.All.newConnectedUser(Clients.Caller.userName);
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            Clients.All.disconnectedUser(Clients.Caller.userName);
+            return base.OnDisconnected(stopCalled);
+        }
+    }
+}
